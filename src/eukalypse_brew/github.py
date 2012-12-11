30704ef01cb1b53f136a1f0df6d0117cde40f377
Dennis Schwertel <s@digitalkultur.net>
2012-12-11 17:24:39 +0100

    first working github readme check

--- src/eukalypse_brew/github.py
+++ src/eukalypse_brew/github.py
@@ -13,25 +13,26 @@ class Github:
     def _create_url(self, path=None):
         return "http://github.com/{0}/{1}/".format(self.user, self.project)
 
-    def check_readme(self, path=None):
-        """If path is not set, root path is used"""
-        response = requests.get(self._create_url(path))
+    def check_readme(self):
+        """Checks if the README of a project on github got any broken links"""
+        response = requests.get(self._create_url())
         if not response.status_code == 200:
-            raise Exception("url ({0}) response not 200".format(self._create_url(path)))
+            raise Exception("url ({0}) response not 200".format(self._create_url()))
         soup = BeautifulSoup(response.content)
         a_elements = soup.find(id="readme").find_all('a')
         img_elements = soup.find(id="readme").find_all('img')
         for a_element in a_elements:
             link = a_element.get('href')
-            if not link.startswith('#') and not link == self._create_url(path):
+            if not link.startswith('#') and not link == self._create_url():
                 response = requests.get(link)
                 if not response.status_code == 200:
-                    raise Exception("url ({0}) response {1}".format(link,response.status_code))
+                    raise Exception("url ({0}) response {1}".format(link, response.status_code))
 
         for img_element in img_elements:
-            link = a_element.get('src')
-            response = requests.get(link)
-            if not response.status_code == 200:
-                raise Exception("url ({0}) response is {1}".format(link,response.status_code))
-
-        raise Exception("ar")
+            link = img_element.get('src')
+            if link:
+                response = requests.get(link)
+                if not response.status_code == 200:
+                    raise Exception("url ({0}) response is {1}".format(link, response.status_code))
+            else:
+                raise Exception("found image without or empty src attribute {0}".format(img_element))
