63f687a3c4814933fb44beb8424596a2f93e5a8c
Dennis Schwertel <s@digitalkultur.net>
2012-12-13 16:19:36 +0100

    doc

--- src/eukalypse_brew/github.py
+++ src/eukalypse_brew/github.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 
 
 class Github:
+    """
+    sanity checks for projects on Github
+    """
 
     readme_locator = "#readme .entry-content"
 
@@ -14,7 +17,7 @@ class Github:
         return "http://github.com/{0}/{1}/".format(self.user, self.project)
 
     def check_readme(self):
-        """Checks if the README of a project on github got any broken links"""
+        """Checks if the README of a project on github got any broken links in a tags or images"""
         response = requests.get(self._create_url())
         if not response.status_code == 200:
             raise Exception("url ({0}) response not 200".format(self._create_url()))
